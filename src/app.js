e77535d97cbec865eab0af2771c7b7a1a7df916e
biggs <realman_1994@outlook.com>
2016-10-28 11:27:24 +0800

    sourcemaps

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-import nimei from './nimei';
 import Koa from 'koa'
 const app = new Koa()
 // response
 app.use(async (ctx) => {
-  nimei(234)
   ctx.body = 'Hello World'
 })
 
 app.listen(3000, () => console.log('server started 3000'))
+export default app
