72f04893805c9adf4be6301148e5bd522cee78d9
biggs <realman_1994@outlook.com>
2016-10-28 11:25:18 +0800

    debug

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = new Koa()
 // response
 app.use(async (ctx) => {
   nimei(234)
-  ctx.body = 'Hello WorDD44FF4ld1'
+  ctx.body = 'Hello World'
 })
 
 app.listen(3000, () => console.log('server started 3000'))
